1ec59defd9d2bb1f6d3d31ee900ebaf222b6dcd9
samuelzewde <32627331+samuelzewde@users.noreply.github.com>
2022-12-15 10:24:06 -0500

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,15 +6,15 @@
 */
 void print_diagonal(int n)
 {
-	int x, i;
+	int i, j;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (x = 0; x < n; x++)
+		for (i = 0; i < n; i++)
 		{
-			for (i = 0; i < x; i++)
+			for (j = 0; j < i; j++)
 				_putchar(' ');
 			_putchar(92);
 			_putchar('\n');
