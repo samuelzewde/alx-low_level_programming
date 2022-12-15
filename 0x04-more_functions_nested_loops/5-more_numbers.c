45edf48716b3055fbd72a2d0e0fda65a60bfb19e
samuelzewde <32627331+samuelzewde@users.noreply.github.com>
2022-12-15 08:44:56 -0500

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,9 @@ void more_numbers(void)
 {
 	int i, j;
 	
-	for (int i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (int j = 0; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 			_putchar(j + '0');
 		_putchar('\n');
 	}
