ffd7e2e562b5951c333cb1d11c4159d3d252a8b4
samuelzewde <samuelzewde99@gmail.com>
2022-12-15 10:02:56 -0500

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_line - print a straight line
+* @n: number of _ 
+*/
+void print_line(int n)
+{
+	int i;
+
+	if(n <= 0)
+		_putchar('\n');
+	else
+	{
+		for(i = 0; i <= n; i++)
+			_putchar(95);
+		_putchar('\n');
+	}
+}
