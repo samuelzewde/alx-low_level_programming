baa547d304fd0f9ba0f2f278e1714db16fa4bba2
samuelzewde <32627331+samuelzewde@users.noreply.github.com>
2022-12-15 10:21:23 -0500

    commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
-* print_diagonal - print diagonal line
-* @n: integer
+* print_diagonal -> printing line
+* @n: integer params
 */
 void print_diagonal(int n)
 {
-	int i, j;
+	int x, j;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (y = 0; y < n; y++)
 		{
-			for (j = 0; j < i; j++)
+			for (j = 0; j < y; i++)
 				_putchar(' ');
 			_putchar(92);
 			_putchar('\n');
