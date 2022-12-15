a0901997bb3b8c6870297d8eb1e6ff9779163953
samuelzewde <samuelzewde99@gmail.com>
2022-12-15 07:34:20 -0500

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* _isdigit - check digit
+* @c: parameter to _isupper
+* Return: 0 or 1
+*/
+int _isdigit(int c)
+{
+	if((c >= '0') && (c <= '9'))
+		return (1);
+	else
+		return (0);
+{
