f9bb2f719fb787acf87541735c77998afff488a6
samuelzewde <32627331+samuelzewde@users.noreply.github.com>
2022-12-19 20:16:46 -0500

    commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - set n value to 98
- * @n: int type pointer
- * Return: void
- */
- 
-void reset_to_98(int *n)
+* reset_to_98 - set n value to 98
+* @n: int type pointer
+* Return: void
+*/
+
+void rest_to_98(int *n)
 {
 	*n = 98;
 }
